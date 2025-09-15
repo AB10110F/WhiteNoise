005f73ea8909a6c48195156b565a2e68a34f4737
A. Bryant <W.Sword.09@hotmail.com>
2025-09-15 01:12:05 -0600

    Chore: add components directory

---
+++ Views/Components/PlayerControls.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+namespace WhiteNoise.Views.Components;
+
+public partial class PlayerControls : UserControl
+{
+    public PlayerControls()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ Views/Components/SongsList.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+namespace WhiteNoise.Views.Components;
+
+public partial class SongsList : UserControl
+{
+    public SongsList()
+    {
+        InitializeComponent();
+    }
+}
